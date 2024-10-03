6ac155183a9a9cde492a5d16c5894e03f1fa0256
Uriel Amoroso <urielamoroso0318@gmail.com>
2024-10-03 23:35:26 +0800

    added login page

---
+++ lib/features/login.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:unit4_exercise/utils/reusable.dart';
+import 'package:unit4_exercise/utils/styles.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Define the black color
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center, // Remove the extra comma
+            children: [
+              Image.asset('logo.png', height: 200),
+              const SizedBox(height: 20),
+              Text(
+                "TranspoTrack",
+                style: header,
+              ),
+
+              const SizedBox(height: 20),
+              Align(alignment: Alignment.centerLeft,
+              child: Text('Username', style: subheader),
+              ),
+              const SizedBox(height: 10),
+              reusableTextField('Enter Username', Icons.person, false),
+              const SizedBox(height: 20),
+
+              Align(alignment: Alignment.centerLeft,
+              child: Text('Password', style: subheader),
+              ),
+              const SizedBox(height: 10),
+              reusableTextField('Enter Password', Icons.lock_outline_rounded, true),
+
+              Container(width: MediaQuery.of(context).size.width, 
+              height: 50, margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+              ),
+            
+                ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: Text("Login", style: subheader),
+              ),
+            ],
+          ),
+        ),
+        ),
+      ),
+    );
+  }
+}
