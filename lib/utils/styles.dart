462e9ae885ac9ae8df9e6aaa5d2a6e879d3b32bb
Uriel Amoroso <urielamoroso0318@gmail.com>
2024-10-03 23:35:03 +0800

    added text styles and color

---
+++ lib/utils/styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Colors
+var accent = const Color(0xFF022954);
+var heading = const Color(0xFF354c7c);
+var background = const Color(0xFFD9D9D9);
+var white = const Color(0xFFF4F4F4);
+
+// TextStyles
+TextStyle header = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w700, color: accent, fontSize: 36);
+TextStyle subheader = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w700, color: heading, fontSize: 24);
+TextStyle textBold = GoogleFonts.montserrat(
+    fontSize: 14, fontWeight: FontWeight.w700, color: accent);
+TextStyle texts = GoogleFonts.montserrat(
+    fontSize: 14, fontWeight: FontWeight.w400, color: accent);
