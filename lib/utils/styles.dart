5e114a72fd4d8011356361cf0acfb339af487684
Uriel Amoroso <urielamoroso0318@gmail.com>
2024-10-28 22:44:06 +0800

    updated styles

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -6,6 +6,7 @@ var accent = const Color(0xFF022954);
 var heading = const Color(0xFF354c7c);
 var background = const Color(0xFFD9D9D9);
 var white = const Color(0xFFF4F4F4);
+var red = const Color(0xFF800020);
 
 // TextStyles
 TextStyle header = GoogleFonts.montserrat(
@@ -16,3 +17,5 @@ TextStyle textBold = GoogleFonts.montserrat(
     fontSize: 14, fontWeight: FontWeight.w700, color: accent);
 TextStyle texts = GoogleFonts.montserrat(
     fontSize: 14, fontWeight: FontWeight.w400, color: accent);
+TextStyle errorText = GoogleFonts.montserrat(
+    fontSize: 14, fontWeight: FontWeight.w400, color: red);
