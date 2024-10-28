4f71e30147cb3aa344fa13d3a9cb993f73717fa5
Uriel Amoroso <urielamoroso0318@gmail.com>
2024-10-28 22:44:00 +0800

    updated reusables

--- lib/utils/reusable.dart
+++ lib/utils/reusable.dart
@@ -1,13 +1,15 @@
 import "package:flutter/material.dart";
 import "package:unit4_exercise/utils/styles.dart";
 
-TextField reusableTextField(String text, IconData icon, bool isPasswordType,) {
+TextField reusableTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
   return TextField(
+    controller: controller,
     obscureText: isPasswordType,
     enableSuggestions: !isPasswordType,
     autocorrect: !isPasswordType,
-    cursorColor: accent,
-    style: texts,
+    cursorColor: heading,
+    style: textBold,
     decoration: InputDecoration(
       prefixIcon: Icon(
         icon,
@@ -15,15 +17,43 @@ TextField reusableTextField(String text, IconData icon, bool isPasswordType,) {
       ),
       labelText: text,
       labelStyle: texts,
-      filled: false,
+      filled: true,
       floatingLabelBehavior: FloatingLabelBehavior.auto,
       fillColor: white,
       border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(40.0),
-          borderSide: const BorderSide(width: 0, style: BorderStyle.solid)),
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide:
+              BorderSide(width: 1, style: BorderStyle.solid, color: accent)),
     ),
     keyboardType: isPasswordType
         ? TextInputType.visiblePassword
         : TextInputType.emailAddress,
+  );
+}
+
+Container login(BuildContext context, String text, Function onPressed) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: MediaQuery.of(context).size.width * 0.05,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: () => onPressed(context), // Call the onPressed function here
+      child: Text(
+        text,
+        style: textBold,
+      ),
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) {
+            return accent;
+          }
+          return white;
+        }),
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ),
+      ),
+    ),
   );
 }
